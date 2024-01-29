5dfae1818d890b171faaead05a0d7cd06c3af2be
youralmight <youralmight@gmail.com>
2024-01-29 10:37:38 +0800

    Remove third-party dependencies

--- src/third-party/get_some_reference_reps.sh
+++
@@ -1,3 +0,0 @@
-git clone 'git@github.com:youralmight/snippets.git'
-git clone 'https://github.com/github/gitignore'
-git clone 'https://github.com/devcontainers/features'
--- src/third-party/reference_links.md
+++
@@ -1,2 +0,0 @@
-![git-book](https://git-scm.com/book/en/v2)
-![gitignore](https://git-scm.com/docs/gitignore)
